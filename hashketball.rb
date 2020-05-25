85a50a975a59004ecb6a41eff110c9f5d738bf4e
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 10:23:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -138,7 +138,7 @@ def num_points_scored(name)
   }
   stats[:away][:players].each { |player|
     if player[:player_name] == name
-      team = :away
+      score = player[:points]
     end
   }
 end
